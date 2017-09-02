9c0771d6014a798b99174d1b3861164da279cff9
anna-marie-vos <amvos@outlook.com>
2017-09-02 12:27:34 +1200

    webhook function added

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use pimax\FbBotApp;
+use pimax\Messages\Message;
+
 class MessengerController extends Controller
 {
+  public function webhook(){
+    return "this is the webhook";
+  }
     //
 }
